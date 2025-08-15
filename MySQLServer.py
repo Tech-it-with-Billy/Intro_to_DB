224b3f0e5b8685111b6d9d6c9c2666877ad18002
Billy Och <ochiengbilly02@gmail.com>
2025-08-15 21:12:35 +0300

    Update: adjusting the connection error function

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ def create_connection(host_name, user_name, user_password):
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully")
         
-    except Error as e:
+    except mysql.connector.Error e:
         print(f"The error '{e}' occurred")
     
     finally:
